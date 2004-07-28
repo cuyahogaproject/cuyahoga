42004f84bc0392c79692ae60c8852ad1fceeb8ad
= Martijn Boland <martijn@boland.org>
2004-07-28 21:49:44 +0000

    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@41 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Modules/Articles/Article.cs
+++ Modules/Articles/Article.cs
@@ -1,5 +1,7 @@
 using System;
 
+using Cuyahoga.Core;
+
 namespace Cuyahoga.Modules.Articles
 {
 	/// <summary>
@@ -7,6 +9,30 @@ namespace Cuyahoga.Modules.Articles
 	/// </summary>
 	public class Article
 	{
+		private int _id;
+		private int _sectionId;
+		private int _createdById;
+		private int _modifiedById;
+		private int _categoryId;
+		private string _title;
+		private string _summary;
+		private string _content;
+		private bool _syndicate;
+		private DateTime _dateOnline;
+		private DateTime _dateOffline;
+		private DateTime _created;
+		private DateTime _updated;
+		private Section _section;
+		private User _createdBy;
+		private User _modifiedBy;
+		private Category _category;
+		private IList _comments;
+
+		#region properties
+
+
+		#endregion
+
 		public Article()
 		{
 			//
--- Modules/User/UserModule.cs
+++ Modules/User/UserModule.cs
@@ -8,7 +8,8 @@ namespace Cuyahoga.Modules.User
 	/// The UserModule provides services for user management, specifically non-admin users.
 	/// <remark>
 	/// Note that the core user functionality is not in this module but in Cuyahoga.Core and
-	/// Cuyahoga.Web.Util.AuthenticationModule.
+	/// Cuyahoga.Web.Util.AuthenticationModule. This class is needed because every module requires 
+	/// a corresponding class. Perhaps functionality like 'forgot password?' will be added here in the future.
 	/// </remark>
 	/// </summary>
 	public class UserModule : Module
