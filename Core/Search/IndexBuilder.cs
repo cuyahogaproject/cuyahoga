15fadbe14f52f36cd91cce2c0e0577f9851f19fc
= Martijn Boland <martijn@boland.org>
2005-02-09 23:02:50 +0000

    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@201 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Core/Search/IndexBuilder.cs
+++ Core/Search/IndexBuilder.cs
@@ -1,44 +1,142 @@
 using System;
 
+using Lucene.Net.Index;
+using Lucene.Net.Documents;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Store;
+
+using Cuyahoga.Core.Util;
+
 namespace Cuyahoga.Core.Search
 {
 	/// <summary>
-	/// Summary description for IndexBuilder.
+	/// The IndexBuilder class takes care of maintaining the fulltext index.
 	/// </summary>
-	public class IndexBuilder
+	public class IndexBuilder : IDisposable
 	{
+		private Directory _indexDirectory;
+		private IndexWriter _indexWriter;
+		private bool _isClosed = false;
+		private bool _rebuildIndex;
+
 		/// <summary>
-		/// The IndexBuilder class takes care of maintaining the Full-Text index.
+		/// Default constructor.
 		/// </summary>
-		public IndexBuilder()
+		/// <param name="physicalIndexDir">Location of the index files.</param>
+		/// <param name="rebuildIndex">Flag to indicate if the index should be rebuilt. 
+		/// NOTE: you can not update or delete content when rebuilding the index.
+		/// </param>
+		public IndexBuilder(string physicalIndexDir, bool rebuildIndex)
 		{
-			//
-			// TODO: Add constructor logic here
-			//
+			this._indexDirectory = FSDirectory.GetDirectory(physicalIndexDir, true);
+			this._rebuildIndex = rebuildIndex;
+
+			InitIndexWriter();
 		}
 
 		/// <summary>
-		/// 
+		/// Add content to be indexed.
 		/// </summary>
 		/// <param name="searchContent"></param>
 		public void AddContent(SearchContent searchContent)
 		{
+			if (this._indexWriter == null)
+			{
+				InitIndexWriter();
+			}
+			this._indexWriter.AddDocument(BuildDocumentFromSearchContent(searchContent));
 		}
 
 		/// <summary>
-		/// 
+		/// Update existing content in the search index.
 		/// </summary>
 		/// <param name="searchContent"></param>
 		public void UpdateContent(SearchContent searchContent)
 		{
+			if (this._rebuildIndex)
+			{
+				throw new InvalidOperationException("Cannot update documents when rebuilding the index.");
+			}
+			else
+			{
+				// First delete the old content
+				DeleteContent(searchContent);
+				// Now add the content again
+				AddContent(searchContent);
+			}
 		}
 
 		/// <summary>
-		/// 
+		/// Delete existing content from the search index.
 		/// </summary>
 		/// <param name="searchContent"></param>
 		public void DeleteContent(SearchContent searchContent)
 		{
+			if (this._rebuildIndex)
+			{
+				throw new InvalidOperationException("Cannot delete documents when rebuilding the index.");
+			}
+			else
+			{
+				this._indexWriter.Close();
+				this._indexWriter = null;
+
+				// The path uniquely identifies a document in the index.
+				Term term = new Term("path", searchContent.Path);
+				IndexReader rdr = IndexReader.Open(this._indexDirectory);
+				rdr.Delete(term);
+			}
 		}
+
+		/// <summary>
+		/// Close the IndexWriter (saves the index).
+		/// </summary>
+		public void Close()
+		{
+			if (! this._isClosed)
+			{
+				this._indexWriter.Optimize();
+				this._indexWriter.Close();
+				this._isClosed = true; 
+			}
+		}
+
+		private void InitIndexWriter()
+		{
+			if (! IndexReader.IndexExists(this._indexDirectory) || this._rebuildIndex)
+			{
+				this._indexWriter = new IndexWriter(this._indexDirectory, new StandardAnalyzer(), true);
+			}
+			else
+			{
+				this._indexWriter = new IndexWriter(this._indexDirectory, new StandardAnalyzer(), false);
+			}
+		}
+
+		private Document BuildDocumentFromSearchContent(SearchContent searchContent)
+		{
+			Document doc = new Document();
+			doc.Add(Field.Text("title", searchContent.Title));
+			doc.Add(Field.Text("summary", searchContent.Summary));
+			doc.Add(Field.UnStored("contents", searchContent.Contents));
+			doc.Add(Field.Text("author", searchContent.Author));
+			doc.Add(Field.Keyword("moduletype", searchContent.ModuleType));
+			doc.Add(Field.Keyword("path", searchContent.Path));
+			doc.Add(Field.Keyword("category", searchContent.Category));
+			doc.Add(Field.Keyword("datecreated", DateField.DateToString(searchContent.DateCreated)));
+			doc.Add(Field.Keyword("datemodified", DateField.DateToString(searchContent.DateModified)));
+			doc.Add(Field.UnIndexed("sectionid", searchContent.SectionId.ToString()));
+
+			return doc;
+		}
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			Close();
+		}
+
+		#endregion
 	}
 }
--- Core/Search/SearchContent.cs
+++ Core/Search/SearchContent.cs
@@ -3,15 +3,116 @@ using System;
 namespace Cuyahoga.Core.Search
 {
 	/// <summary>
-	/// Summary description for SearchContent.
+	/// Class that holds the content to be indexed for a single document.
 	/// </summary>
 	public class SearchContent
 	{
+		private string _title;
+		private string _summary;
+		private string _contents;
+		private string _author;
+		private string _moduleType;
+		private string _path;
+		private string _category;
+		private DateTime _dateCreated;
+		private DateTime _dateModified;
+		private int _sectionId;
+
+		/// <summary>
+		/// Property Title (string)
+		/// </summary>
+		public string Title
+		{
+			get { return this._title; }
+			set { this._title = value; }
+		}
+
+		/// <summary>
+		/// Property Summary (string)
+		/// </summary>
+		public string Summary
+		{
+			get { return this._summary; }
+			set { this._summary = value; }
+		}
+
+		/// <summary>
+		/// Property Contents (string)
+		/// </summary>
+		public string Contents
+		{
+			get { return this._contents; }
+			set { this._contents = value; }
+		}
+
+		/// <summary>
+		/// Property Author (string)
+		/// </summary>
+		public string Author
+		{
+			get { return this._author; }
+			set { this._author = value; }
+		}
+
+		/// <summary>
+		/// Property ModuleType (string)
+		/// </summary>
+		public string ModuleType
+		{
+			get { return this._moduleType; }
+			set { this._moduleType = value; }
+		}
+
+		/// <summary>
+		/// Property Path (string)
+		/// </summary>
+		public string Path
+		{
+			get { return this._path; }
+			set { this._path = value; }
+		}
+
+		/// <summary>
+		/// Property Category (string)
+		/// </summary>
+		public string Category
+		{
+			get { return this._category; }
+			set { this._category = value; }
+		}
+
+		/// <summary>
+		/// Property DateCreated (DateTime)
+		/// </summary>
+		public DateTime DateCreated
+		{
+			get { return this._dateCreated; }
+			set { this._dateCreated = value; }
+		}
+
+		/// <summary>
+		/// Property DateModified (DateTime)
+		/// </summary>
+		public DateTime DateModified
+		{
+			get { return this._dateModified; }
+			set { this._dateModified = value; }
+		}
+
+		/// <summary>
+		/// Property SectionId (int)
+		/// </summary>
+		public int SectionId
+		{
+			get { return this._sectionId; }
+			set { this._sectionId = value; }
+		}
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
 		public SearchContent()
 		{
-			//
-			// TODO: Add constructor logic here
-			//
 		}
 	}
 }
