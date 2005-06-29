a407246365cc7254229e64054ac1b763f12c79dc
= Martijn Boland <martijn@boland.org>
2005-06-29 19:38:40 +0000

    Cuyahoga 0.8.1 release
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@295 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Core/AssemblyInfo.cs
+++ Core/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Cuyahoga.Web")]
+[assembly: AssemblyTitle("Cuyahoga.Core")]
 [assembly: AssemblyDescription("A .NET web site framework.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Cuyahoga")]
 [assembly: AssemblyCopyright("Licensed under the BSD license.")]
 
-[assembly: AssemblyVersion("0.8.0.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
--- Web/AssemblyInfo.cs
+++ Web/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("Cuyahoga")]
 [assembly: AssemblyCopyright("Licensed under the BSD license.")]
 
-[assembly: AssemblyVersion("0.8.0.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
 
 [assembly: log4net.Config.DOMConfigurator()]
