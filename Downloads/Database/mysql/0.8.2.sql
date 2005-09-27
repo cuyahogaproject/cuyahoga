f0865a562bca5d551ca640c250588ffdf7e29d5d
= Martijn Boland <martijn@boland.org>
2005-09-27 20:37:34 +0000

    Changed dir structure
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@368 cb474c29-17db-0310-ba2e-db2fec5ab780

---
+++ Downloads/Database/mysql/0.8.2.sql
@@ -0,0 +1,49 @@
+/*
+ *  Table definition
+ */
+
+CREATE TABLE cm_file(
+fileid INT NOT NULL AUTO_INCREMENT,
+sectionid INT NOT NULL,
+publisherid INT NOT NULL,
+filepath VARCHAR(255) NOT NULL,
+title VARCHAR(100),
+filesize INT NOT NULL,
+nrofdownloads INT NOT NULL,
+contenttype VARCHAR(50) NOT NULL,
+datepublished DATETIME NOT NULL,
+inserttimestamp TIMESTAMP NOT NULL,
+updatetimestamp DATETIME NOT NULL,
+FOREIGN KEY (sectionid) REFERENCES cuyahoga_section (sectionid),
+FOREIGN KEY (publisherid) REFERENCES cuyahoga_user (userid),
+PRIMARY KEY (fileid));
+
+
+CREATE TABLE cm_filerole(
+fileroleid INT NOT NULL AUTO_INCREMENT,
+fileid INT NOT NULL,
+roleid INT NOT NULL,
+FOREIGN KEY (fileid) REFERENCES cm_file (fileid),
+FOREIGN KEY (roleid) REFERENCES cuyahoga_role (roleid),
+PRIMARY KEY (fileroleid));
+
+
+/*
+ *  Table data
+ */
+
+INSERT INTO cuyahoga_moduletype (name, assemblyname, classname, path, editpath, inserttimestamp, updatetimestamp)
+VALUES ('Downloads', 'Cuyahoga.Modules.Downloads', 'Cuyahoga.Modules.Downloads.DownloadsModule', 'Modules/Downloads/Downloads.ascx', 'Modules/Downloads/EditDownloads.aspx', '2005-05-15 14:36:28.324', '2004-05-15 14:36:28.324');
+
+SELECT @moduletypeid := last_insert_id();
+
+INSERT INTO cuyahoga_modulesetting (moduletypeid, name, friendlyname, settingdatatype, iscustomtype, isrequired)
+VALUES (@moduletypeid, 'SHOW_PUBLISHER', 'Show publisher', 'System.Boolean', 0, 1);
+
+INSERT INTO cuyahoga_modulesetting (moduletypeid, name, friendlyname, settingdatatype, iscustomtype, isrequired)
+VALUES (@moduletypeid, 'SHOW_DATE', 'Show file date', 'System.Boolean', 0, 1);
+
+INSERT INTO cuyahoga_modulesetting (moduletypeid, name, friendlyname, settingdatatype, iscustomtype, isrequired)
+VALUES (@moduletypeid, 'PHYSICAL_DIR', 'Physical directory (empty for App_Root/files)', 'System.String', 0, 0);
+
+INSERT INTO cuyahoga_version (assembly, major, minor, patch) VALUES ('Cuyahoga.Modules.Downloads', 0, 8, 2);
