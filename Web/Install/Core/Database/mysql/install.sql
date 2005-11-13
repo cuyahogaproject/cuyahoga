ca44e297550f59afc98fa0e3dd6f7db3f938c903
= Martijn Boland <martijn@boland.org>
2005-11-13 16:00:38 +0000

    Database install fixes for the 0.9.1 release
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@402 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Web/Install/Core/Database/mysql/install.sql
+++ Web/Install/Core/Database/mysql/install.sql
@@ -178,7 +178,7 @@ CREATE TABLE cuyahoga_sectionconnection(
 sectionconnectionid INT NOT NULL AUTO_INCREMENT,
 sectionidfrom INT NOT NULL,
 sectionidto INT NOT NULL,
-actionname VARCHAR(50) NOT NULL
+actionname VARCHAR(50) NOT NULL,
 FOREIGN KEY (sectionidfrom) REFERENCES cuyahoga_section (sectionid),
 FOREIGN KEY (sectionidto) REFERENCES cuyahoga_section (sectionid),
 PRIMARY KEY (sectionconnectionid),
