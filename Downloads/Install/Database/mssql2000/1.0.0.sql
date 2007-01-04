a17ede15a01d7ab61fa2915d191945cc0c27942e
Martijn Boland <martijn@boland.org>
2007-01-04 16:17:23 +0000

    Renamed physical directory of Downloads module to Cuyahoga.Modules.Downloads
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@535 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Downloads/Install/Database/mssql2000/1.0.0.sql
+++
@@ -1,2 +0,0 @@
-UPDATE cuyahoga_version SET major = 1, minor = 0, patch = 0 WHERE assembly = 'Cuyahoga.Modules.Downloads'
-go
--- Downloads/Install/Database/postgresql/1.0.0.sql
+++
@@ -1,1 +0,0 @@
-UPDATE cuyahoga_version SET major = 1, minor = 0, patch = 0 WHERE assembly = 'Cuyahoga.Modules.Downloads';
