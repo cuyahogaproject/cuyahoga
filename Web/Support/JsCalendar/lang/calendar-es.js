c3abb06693f4a1cba721cc83d8c57d09c31b1613
Martijn Boland <martijn@boland.org>
2007-02-02 22:43:26 +0000

    [CUY-114] Updated spanish language file for js calendar.
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@563 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Web/Support/JsCalendar/lang/calendar-es.js
+++ Web/Support/JsCalendar/lang/calendar-es.js
@@ -1,10 +1,8 @@
 // ** I18N
 
-// Calendar ES (spanish) language
+// Calendar EN language
 // Author: Mihai Bazon, <mihai_bazon@yahoo.com>
-// Updater: Servilio Afre Puentes <servilios@yahoo.com>
-// Updated: 2004-06-03
-// Encoding: utf-8
+// Encoding: any
 // Distributed under the same terms as the calendar itself.
 
 // For translators: please use UTF-8 if possible.  We strongly believe that
@@ -16,10 +14,10 @@ Calendar._DN = new Array
 ("Domingo",
  "Lunes",
  "Martes",
- "Miércoles",
+ "Mi&eacute;",
  "Jueves",
  "Viernes",
- "Sábado",
+ "S&aacute;bado",
  "Domingo");
 
 // Please note that the following array of short day names (and the same goes
@@ -39,15 +37,15 @@ Calendar._SDN = new Array
 ("Dom",
  "Lun",
  "Mar",
- "Mié",
+ "Mi&eacute;",
  "Jue",
  "Vie",
- "Sáb",
+ "S&aacute;b",
  "Dom");
 
 // First day of the week. "0" means display Sunday first, "1" means display
 // Monday first, etc.
-Calendar._FD = 1;
+Calendar._FD = 0;
 
 // full month names
 Calendar._MN = new Array
@@ -84,33 +82,25 @@ Calendar._TT = {};
 Calendar._TT["INFO"] = "Acerca del calendario";
 
 Calendar._TT["ABOUT"] =
-"Selector DHTML de Fecha/Hora\n" +
-"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
-"Para conseguir la última versión visite: http://www.dynarch.com/projects/calendar/\n" +
-"Distribuido bajo licencia GNU LGPL. Visite http://gnu.org/licenses/lgpl.html para más detalles." +
-"\n\n" +
-"Selección de fecha:\n" +
-"- Use los botones \xab, \xbb para seleccionar el año\n" +
-"- Use los botones " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " para seleccionar el mes\n" +
-"- Mantenga pulsado el ratón en cualquiera de estos botones para una selección rápida.";
+"Selector DHTML de Fecha/Hora.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"Selección de hora:\n" +
+"Selecci&aacute;n de hora:\n" +
 "- Pulse en cualquiera de las partes de la hora para incrementarla\n" +
-"- o pulse las mayúsculas mientras hace clic para decrementarla\n" +
-"- o haga clic y arrastre el ratón para una selección más rápida.";
+"- o pulse las may&uacute;sculas mientras hace clic para decrementarla\n" +
+"- o haga clic y arrastre el rat&oacute;n para una selecci&oacute;n m&aacute;s r&aacute;pida.";
 
-Calendar._TT["PREV_YEAR"] = "Año anterior (mantener para menú)";
-Calendar._TT["PREV_MONTH"] = "Mes anterior (mantener para menú)";
+Calendar._TT["PREV_YEAR"] = "Año anterior (mantener para men&uacute;)";
+Calendar._TT["PREV_MONTH"] = "Mes anterior (mantener para men&uacute;)";
 Calendar._TT["GO_TODAY"] = "Ir a hoy";
-Calendar._TT["NEXT_MONTH"] = "Mes siguiente (mantener para menú)";
-Calendar._TT["NEXT_YEAR"] = "Año siguiente (mantener para menú)";
+Calendar._TT["NEXT_MONTH"] = "Mes siguiente (mantener para men&uacute;)";
+Calendar._TT["NEXT_YEAR"] = "Año siguiente (mantener para men&uacute;)";
 Calendar._TT["SEL_DATE"] = "Seleccionar fecha";
 Calendar._TT["DRAG_TO_MOVE"] = "Arrastrar para mover";
 Calendar._TT["PART_TODAY"] = " (hoy)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "Hacer %s primer día de la semana";
+Calendar._TT["DAY_FIRST"] = "Hacer %s primer d&iacute;a de la semana";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
@@ -119,7 +109,7 @@ Calendar._TT["WEEKEND"] = "0,6";
 
 Calendar._TT["CLOSE"] = "Cerrar";
 Calendar._TT["TODAY"] = "Hoy";
-Calendar._TT["TIME_PART"] = "(Mayúscula-)Clic o arrastre para cambiar valor";
+Calendar._TT["TIME_PART"] = "(May&uacute;scula-)Clic o arrastre para cambiar valor";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%d/%m/%Y";
