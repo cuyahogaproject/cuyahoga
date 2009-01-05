ed6a923cde1e5a7321df49dc79b88118efb5b81d
Martijn Boland <martijn@boland.org>
2009-01-05 11:18:13 +0000

    Recreating test project.
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@743 cb474c29-17db-0310-ba2e-db2fec5ab780

---
+++ Tests/Core/Domain/NodeTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Cuyahoga.Tests.Core.Domain
+{
+	[TestFixture]
+	public class NodeTests
+	{
+	}
+}
