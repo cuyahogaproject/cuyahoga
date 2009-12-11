bb8993c087cda946685fe19918f2563651085de1
Martijn Boland <martijn@boland.org>
2009-12-11 10:22:55 +0000

    Removed legacy database scripts.
    git-svn-id: https://cuyahoga.svn.sourceforge.net/svnroot/cuyahoga/trunk@903 cb474c29-17db-0310-ba2e-db2fec5ab780

--- Database/mssql2000/upgradedb-0.5-1-0.6-mssql2000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO cuyahoga_moduletype (name, assemblyname, classname, path, editpath, inserttimestamp, updatetimestamp) VALUES ('Search', 'Cuyahoga.Modules', 'Cuyahoga.Modules.Search.SearchModule', 'Modules/Search/Search.ascx', NULL, '2004-10-02 14:36:28.324', '2004-10-02 14:36:28.324')
-INSERT INTO cuyahoga_moduletype (name, assemblyname, classname, path, editpath, inserttimestamp, updatetimestamp) VALUES ('LanguageSwitcher', 'Cuyahoga.Modules', 'Cuyahoga.Modules.LanguageSwitcher.LanguageSwitcherModule', 'Modules/LanguageSwitcher/LanguageSwitcher.ascx', NULL, '2004-10-02 14:36:28.324', '2004-10-02 14:36:28.324')
-
-GO
--- Database/postgresql/upgradedb-0.5-0.5.1-postgresql.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO cuyahoga_modulesetting (moduletypeid, name, friendlyname, settingdatatype, iscustomtype, isrequired) VALUES (2, 'ALLOW_SYNDICATION', 'Allow syndication', 'System.Boolean', false, true);
-
-INSERT INTO cuyahoga_sectionsetting (sectionid, name, value)
-	SELECT sectionid, 'ALLOW_SYNDICATION', 'True'
-	FROM cuyahoga_section s
-		INNER JOIN cuyahoga_moduletype m on m.moduletypeid = s.moduletypeid
-	WHERE m.Name = 'Articles';
